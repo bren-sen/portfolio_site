70f9c537c9cdbecf18cd6db045ffa53e556eea7d
Bren-Sen <bren.senden@gmail.com>
2025-03-17 19:01:45 +0000

    Add js for projects focus view

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//nav bar dropdown
 const dropButton = document.getElementById('dropbtn');
 const dropContent = document.getElementById('dropdown-content')
 
@@ -11,3 +12,28 @@ document.addEventListener('click', Event => {
         dropContent.classList.add('hide');
     };
 });
+
+//project focus  to improve: 1 function for all projects
+const proj = document.getElementById('project1');
+
+const projFocus = () => {
+    if (document.getElementById('focus-box').style.opacity == 0) {
+        document.getElementById('focus-box').style.opacity= '0.7';
+        document.getElementById('focus-box').style.zIndex= '2';
+        document.getElementById('project1').style.zIndex= '3';
+    } else {
+        document.getElementById('focus-box').style.opacity= '0';
+        document.getElementById('focus-box').style.zIndex= '-1';
+        document.getElementById('project1').style.zIndex= '1';
+    }
+};
+
+proj.addEventListener('click', projFocus);
+
+document.addEventListener('click', Event => {
+    if (!proj.contains(Event.target)) {
+        document.getElementById('focus-box').style.opacity= '0';
+        document.getElementById('focus-box').style.zIndex= '-1';
+        document.getElementById('project1').style.zIndex= '1';
+    }
+})
